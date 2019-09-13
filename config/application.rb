496d9c2473305c3011628ace2105a16d427bef6a
Fabiano Pavan <fabiano.pavan@soluzionipa.it>
2019-09-13 14:43:46 +0200

    Ricreata master key e cambiate conf per rack cors

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,22 @@ module Tributi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :it
+
     config.action_dispatch.default_headers = {'X-Frame-Options' => 'ALLOWALL'}
 
     config.session_store :cookie_store, key: '_app_session', expire_after: 30.minutes
+
+    #CORS
+    #allow GET, POST or OPTIONS requests from any origin on any resource.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
